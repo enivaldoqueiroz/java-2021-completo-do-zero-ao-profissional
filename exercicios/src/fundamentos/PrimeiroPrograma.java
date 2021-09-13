332c5c6fa3e00c4a637c508eb3037f0bad154870
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-09-13 14:05:52 -0300

    Comentarios

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,9 +1,28 @@
 package fundamentos;
 
+/**
+ * 
+ * @author Enivaldo_Queiroz
+ * @since JDK1.0
+ */
+
 public class PrimeiroPrograma {
 	
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		System.out.println("Iniciando Curso - Primeiro programa!!!"); 
+		
+		//Uma senteça de código termina com:
+		System.out.println("Iniciando Curso - Primeiro programa parte 01!!!"); 
+		
+		/*
+		 * Linha 1
+		 * Linha 2
+		 * Linha 3 ...
+		 * */
+		System.out.println("Iniciando Curso - Primeiro programa parte 02!!!");
 	}
 
 }
