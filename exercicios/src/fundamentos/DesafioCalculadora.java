0e1e2c92201cc88ca65c0b9bcf8954287145c070
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-10-26 23:49:10 -0300

    Desafio Calculadora

---
+++ exercicios/src/fundamentos/DesafioCalculadora.java
@@ -0,0 +1,35 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class DesafioCalculadora {
+
+	public static void main(String[] args) {
+		// TODO Desafio Calculadora
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Informe o número:");
+		double num1 = entrada.nextDouble();
+		
+		System.out.println("Informe o operador: ");
+		String op = entrada.next();
+		
+		System.out.println("Informe número:");
+		double num2 = entrada.nextDouble();
+		
+		//Logica
+		
+		double resultado = "+".equals(op) ? num1 + num2 : 0;
+		resultado = "-".equals(op) ? num1 - num2: resultado;
+		resultado = "*".equals(op) ? num1 * num2: resultado;
+		resultado = "/".equals(op) ? num1 / num2: resultado;
+		resultado = "%".equals(op) ? num1 % num2: resultado;
+		
+		System.out.printf("%.2f %s %.2f = %.2f", num1, op, num2, resultado);
+		
+		entrada.close();		
+
+	}
+
+}
