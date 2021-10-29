2ef2f5856dfe6bddbfb378feec11761ee9a46aa8
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-10-29 20:49:59 -0300

    WhileDetermido

---
+++ exercicios/src/estruturadecontrole/WhileDetermido.java
@@ -0,0 +1,17 @@
+package estruturadecontrole;
+
+public class WhileDetermido {
+
+	public static void main(String[] args) {
+		// TODO WhileDetermido
+		
+		int contador = 0; 
+		
+		while(contador <= 20) {
+			System.out.printf("i = %d\n", contador);
+			contador += 2;
+		}
+
+	}
+
+}
