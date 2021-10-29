eac3975b4b9463c31dcd4238de5edba63941cc61
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-10-29 20:34:04 -0300

    Outra forma de resolver o desafio dia da Semana

--- exercicios/src/estruturadecontrole/DesafioDiaDaSemana.java
+++ exercicios/src/estruturadecontrole/DesafioDiaDaSemana.java
@@ -12,7 +12,7 @@ public class DesafioDiaDaSemana {
 		System.out.print("Digite o dia da semana: ");
 		String diaDaSemana = entrada.nextLine().toUpperCase();
 		
-		if(diaDaSemana.equals("DOMINGO")) {
+		/*if(diaDaSemana.equals("DOMINGO")) {
 			System.out.println(1);
 		} else if(diaDaSemana.equals("SEGUNDA")) {
 			System.out.println(2);
@@ -26,6 +26,26 @@ public class DesafioDiaDaSemana {
 			System.out.println(6);
 		} else if(diaDaSemana.equals("SABADO")) {
 			System.out.println(7);
+		}*/
+		
+		//Outra forma de resolver o desafio dia da Semana
+		
+		if("domingo".equalsIgnoreCase(diaDaSemana)) {
+			System.out.println(1);
+		} else if("segunda".equalsIgnoreCase(diaDaSemana)) {
+			System.out.println(2);
+		} else if("terça".equalsIgnoreCase(diaDaSemana) || "terca".equalsIgnoreCase(diaDaSemana)) {
+			System.out.println(3);
+		} else if("quarta".equalsIgnoreCase(diaDaSemana)) {
+			System.out.println(4);
+		} else if("quinta".equalsIgnoreCase(diaDaSemana)) {
+			System.out.println(5);
+		} else if("sexta".equalsIgnoreCase(diaDaSemana)) {
+			System.out.println(6);
+		} else if("sábado".equalsIgnoreCase(diaDaSemana) || "sabado".equalsIgnoreCase(diaDaSemana)) {
+			System.out.println(7);
+		} else {
+			System.out.println("Dia inválido!");
 		}
 
 	}
