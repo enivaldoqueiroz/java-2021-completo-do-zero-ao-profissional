24adbf6b5a56e07899bf324a8c8813dd85735e5d
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-10-29 20:56:49 -0300

    Estrutura de repetição For

---
+++ exercicios/src/estruturadecontrole/ForUm.java
@@ -0,0 +1,20 @@
+package estruturadecontrole;
+
+public class ForUm {
+
+	public static void main(String[] args) {
+		// TODO Estrutura de repetição For
+		
+		for(int contador = 0; contador <= 10; contador++) {
+			System.out.printf("i = %d\n", contador);
+		}
+// Paracido com while - Não devemos fazer desta forma		
+//		int x = 2;
+//		for(;x < 10;) {
+//			System.out.println("x = " + x);
+//			x++;
+//		}
+
+	}
+
+}
