ce6fb0922a7b478d048f36809a7e2916ef0024b7
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-10-30 17:40:31 -0300

    Implentando construtores

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -1,9 +1,32 @@
 package classe;
 
+import java.io.PrintStream;
+
 public class Data {
 	
 	int dia;
 	int mes;
 	int ano;
+	
+	Data(){
+		dia = 1;
+		mes = 1;
+		ano = 1970;
+	}
+	
+	Data(int diaInicial, int mesInicial, int anoInicial){
+		dia = diaInicial;
+		mes = mesInicial;
+		ano = anoInicial;
+	}
+	
+	String obterDataFormatada(){
+		return String.format("%d/%d/%d\n", dia, mes, ano);
+		
+	}
+	
+	void imprimirDataFormatada() {
+		System.out.printf("\nData: %d/%d/%d", dia, mes, ano);
+	}
 
 }
--- exercicios/src/classe/DataTeste.java
+++ exercicios/src/classe/DataTeste.java
@@ -6,12 +6,18 @@ public class DataTeste {
 		// TODO Desafio Data
 
 		Data data1 = new Data();
-		data1.dia = 3;
-		data1.mes = 11;
-		data1.ano = 1991;
+//		data1.dia = 3;
+//		data1.mes = 11;
+//		data1.ano = 1991;
 		
-		System.out.println("Data: " + data1.dia +"/" + data1.mes +"/"+ data1.ano);
-		System.out.printf("Data: %d/%d/%d", data1.dia, data1.mes, data1.ano);
+		Data data2 = new Data(21 , 6, 1990);
+		
+		
+		//System.out.println("Data: " + data1.dia +"/" + data1.mes +"/"+ data1.ano);
+		//System.out.printf("Data: %d/%d/%d", data1.dia, data1.mes, data1.ano);
+		//System.out.printf("Data: " + data1.obterDataFormatada());
+		data1.imprimirDataFormatada();
+		data2.imprimirDataFormatada();
 	}
 
 }
