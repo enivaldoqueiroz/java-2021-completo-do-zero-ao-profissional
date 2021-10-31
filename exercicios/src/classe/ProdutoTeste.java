3159e9c087788bc5853ae67ef09f3ba19cadf21a
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-10-30 23:35:39 -0300

    Implementando o desafio membros de Classe vs Instância

--- exercicios/src/classe/ProdutoTeste.java
+++ exercicios/src/classe/ProdutoTeste.java
@@ -7,18 +7,19 @@ public class ProdutoTeste {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Produto p1 = new Produto("Notebook", 4356.89, 0.25);
+		Produto p1 = new Produto("Notebook", 4356.89);
 		//p1.nome = "Notebook";
 		//p1.preco = 4356.89;
-		//p1.desconto = 0.25;
 		
 		var p2 = new Produto();
 		p2.nome = "Caneta Preta";
 		p2.preco = 12.56;
-		p2.desconto = 0.29;
+		//p2.desconto = 0.29;
 		
-		System.out.println(p1.nome);
-		System.out.println(p2.nome);
+		Produto.desconto = 0.50;
+		
+		System.out.println(p1.nome + " " + p1.precoComDesconto());
+		System.out.println(p2.nome + " " + p2.precoComDesconto());
 		
 		//double precoFinal1 = p1.preco * (1 - p1.desconto);
 		//double precoFinal2 = p2.preco * (1 - p2.desconto);
