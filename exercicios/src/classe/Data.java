dd44baa8901f4bd86e10d1b41c3cfc394697f455
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-10-31 01:03:45 -0300

    Utilizando o this

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -9,24 +9,27 @@ public class Data {
 	int ano;
 	
 	Data(){
-		dia = 1;
-		mes = 1;
-		ano = 1970;
+		//dia = 1;
+		//mes = 1;
+		//ano = 1970;
+		this(1,1,1970);
 	}
 	
-	Data(int diaInicial, int mesInicial, int anoInicial){
-		dia = diaInicial;
-		mes = mesInicial;
-		ano = anoInicial;
+	//Utilizando o THIS
+	Data(int dia, int mes, int ano){
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
 	}
 	
 	String obterDataFormatada(){
-		return String.format("%d/%d/%d\n", dia, mes, ano);
+		final String formato = "%d%d%d";
+		return String.format(formato, dia, mes, ano);
 		
 	}
 	
 	void imprimirDataFormatada() {
-		System.out.printf("\nData: %d/%d/%d", dia, mes, ano);
+		System.out.printf(this.obterDataFormatada());
 	}
 
 }
