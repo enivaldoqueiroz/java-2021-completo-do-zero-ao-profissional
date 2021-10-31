4bdc02203cd180e7b586333bf3aa13364b8c97e1
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-10-31 00:48:41 -0300

    Trabalhondo como conseito de Classe e Instacia

---
+++ exercicios/src/classe/PrimeiroTrauma.java
@@ -0,0 +1,20 @@
+package classe;
+
+public class PrimeiroTrauma {
+	
+	int a = 3; // não pode mexer na aqui
+	static int b = 4;
+	public static void main(String[] args) {
+		// TODO Primeiro trauma
+		
+		int a = 3;
+		System.out.println(a);
+		
+		PrimeiroTrauma primTrauma = new PrimeiroTrauma(); 
+		System.out.println(primTrauma.a);
+		
+		System.out.println(b);
+
+	}
+
+}
